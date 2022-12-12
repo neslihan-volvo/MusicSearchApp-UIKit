0421cf098eb3c81664f9f294e1753494dcd0c2e9
neslihan-volvo <117173037+neslihan-volvo@users.noreply.github.com>
2022-12-12 09:27:25 +0100

    using delegate

--- MusicSearch_UIKit/DetailViewController.swift
+++ MusicSearch_UIKit/DetailViewController.swift
@@ -1,7 +1,12 @@
 import UIKit
 import AVKit
+protocol DetailViewControllerDelegate: AnyObject {
+  func detailViewController(_ controller: DetailViewController,
+                             openDetailedViewOf musicItem: MusicItemModel)
+}
 
 class DetailViewController: UIViewController {
+    
     
     @IBOutlet weak var musicImage: UIImageView!
     @IBOutlet weak var artistName: UILabel!
@@ -12,34 +17,51 @@ class DetailViewController: UIViewController {
     
     private let musicPlayer = AVPlayer()
     private let imageDownloader = ImageDownloader()
+    weak var delegate: DetailViewControllerDelegate?
     
-    var musicItem: MusicItemModel? {
+    var musicItem : MusicItemModel?
+    /*var musicItem: MusicItemModel? {
         didSet {
             getMusicITemDetails()
         }
-    }
+    }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
         playButton.setTitle("Play", for: .normal)
-        musicImage.image = UIImage(named: "sampleImage100")
-        musicImage.applyShadow(containerView:imageOuterView, coefficient: 0.3)
+        //musicImage.image = UIImage(named: "sampleImage100")
         
-        getMusicITemDetails()
+        //getMusicITemDetails()
+        //getMusicItemDetails(musicItem: musicItem!)
         // Do any additional setup after loading the view.
+        if let item = musicItem {
+            getMusicItemDetails(musicItem: item)
+        }
     }
-    func getMusicITemDetails() {
-        if let musicItem = musicItem,
+    
+    func getMusicItemDetails(musicItem : MusicItemModel) {
+        /*if let musicItem = musicItem,
            let artistName = artistName,
            let trackName = trackName,
            let collectionName = collectionName {
             Task {
                 musicImage.image = await getImage(url: musicItem.artworkUrl60)
             }
+            
+            musicImage.applyShadow(containerView:imageOuterView, coefficient: 0.3)
             artistName.text = musicItem.artistName
             collectionName.text = musicItem.collectionName
             trackName.text = musicItem.trackName
+        }*/
+        Task {
+            musicImage.image = await getImage(url: musicItem.artworkUrl60)
+            
+            musicImage.applyShadow(containerView:imageOuterView, coefficient: 0.3)
         }
+        
+        artistName.text = musicItem.artistName
+        collectionName.text = musicItem.collectionName
+        trackName.text = musicItem.trackName
     }
     func getImage(url: String) async -> UIImage {
         var image = UIImage()
@@ -50,6 +72,26 @@ class DetailViewController: UIViewController {
         }
         return image
     }
+    //    class func detailViewControllerWithItem(_ item: MusicItemModel) -> UIViewController {
+    //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    //
+    //        let viewController =
+    //            storyboard.instantiateViewController(withIdentifier: "MusicDetails")
+    //
+    //        if let detailViewController = viewController as? DetailViewController {
+    //            detailViewController.musicItem = item
+    //        }
+    //
+    //        return viewController
+    //    }
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        super.viewWillAppear(animated)
+    //        artistName.text = musicItem.artistName
+    //        trackName.text = musicItem.trackName
+    //        collectionName.text = musicItem.collectionName
+    //
+    //
+    //    }
     @IBAction func playMusic(_ sender: UIButton) {
         if musicPlayer.rate == 1 {
             musicPlayer.pause()
--- MusicSearch_UIKit/MainViewController.swift
+++ MusicSearch_UIKit/MainViewController.swift
@@ -1,13 +1,19 @@
 import UIKit
 import SwiftUI
 
-class MainViewController: UITableViewController {
+
+
+class MainViewController: UITableViewController, DetailViewControllerDelegate {
     
     var musicItemList = MusicItemModel.getMockMusicItems()
     private let viewModel = ContentViewModel()
     private let imageDownloader = ImageDownloader()
     let searchController = UISearchController(searchResultsController: nil)
     
+    func detailViewController(_ controller: DetailViewController, openDetailedViewOf musicItemModel: MusicItemModel) {
+        //self.musicItem = musicItemModel
+        //getMusicItemDetails(musicItem: musicItemModel)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         searchController.searchResultsUpdater = self
@@ -15,7 +21,7 @@ class MainViewController: UITableViewController {
         searchController.searchBar.placeholder = "Search for Music"
         navigationItem.searchController = searchController
         definesPresentationContext = true
-        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MusicItemIdentifier")
         viewModel.onStateDidChange = { [weak self] in
             self?.onStateDidChange()
         }
@@ -39,8 +45,8 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewModel.state.results?.count ?? 0
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // segue is not used
+    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard
             segue.identifier == "SegueForMusicDetails",
             let indexPath = tableView.indexPathForSelectedRow,
@@ -50,15 +56,32 @@ class MainViewController: UITableViewController {
         }
         let musicItem = viewModel.state.results?[indexPath.row]
         detailViewController.musicItem = musicItem
+    }*/
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SegueForMusicDetails" {
+            let controller = segue.destination as! DetailViewController
+            controller.delegate = self
+            if let indexPath = tableView.indexPathForSelectedRow {
+                controller.musicItem = viewModel.state.results?[indexPath.row]
+            }
+            /*if let indexPath = tableView.indexPath(
+                for: sender as! UITableViewCell) {
+                controller.musicItem = viewModel.state.results?[indexPath.row]
+            }*/
+        }
     }
-    //    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    //        guard let musicItem = viewModel.state.results?[indexPath.row] else {
-    //            fatalError("Oh no should not happen")
-    //        }
-    //        let detailView = DetailViewController.detailViewControllerWithItem(musicItem)
-    //        navigationController?.pushViewController(detailView, animated: true)
-    //        tableView.deselectRow(at: indexPath, animated: true)
-    //    }
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            /*guard let musicItem = viewModel.state.results?[indexPath.row] else {
+                fatalError("Oh no should not happen")
+            }
+            let detailView = DetailViewController.detailViewControllerWithItem(musicItem)
+            navigationController?.pushViewController(detailView, animated: true)*/
+            //let musicItem = viewModel.state.results?[indexPath.row]
+            //delegate?.detailViewController(self, openDetailedViewOf: musicItem!)
+            //Perform a segue via code
+            //performSegue(withIdentifier: "SegueForMusicDetails", sender: nil)
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
@@ -72,6 +95,7 @@ class MainViewController: UITableViewController {
         let artistLabel = cell.viewWithTag(1001) as! UILabel
         artistLabel.text = musicItem.artistName
         let image = cell.viewWithTag(2001) as! UIImageView
+        
         let outerView = cell.viewWithTag(2000)!
         image.adjustsImageSizeForAccessibilityContentSizeCategory = true
         image.applyShadow(containerView: outerView,coefficient: 0.5)
